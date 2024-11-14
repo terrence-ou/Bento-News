907b21dfd0f9130b047a3d1f9634ea92aabbac15
terrence-ou <outerrencedl@gmail.com>
2024-11-13 22:25:16 -0500

    serif font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,15 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        serif: [
+          "Merriweather",
+          "Georgia",
+          "Times New Roman",
+          "Times",
+          "serif",
+        ],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
