473d146c4eec01478eadc2afe277da5b5be457a9
terrence-ou <outerrencedl@gmail.com>
2024-12-03 18:50:08 -0500

    app build config updated

---
+++ forge.config.cjs
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+module.exports = {
+  packagerConfig: {
+    icon: "resources/icon",
+    ignore: [
+      /^\/src/,
+      /(.eslintrc.json)|(.gitignore)|(electron.vite.config.ts)|(forge.config.cjs)|(tsconfig.*)/,
+    ],
+    osxSign: {},
+    osxNotarize: {
+      tool: "notarytool",
+      appleId: process.env.VITE_APPLE_ID,
+      appleIdPassword: process.env.VITE_APPLE_PASSWORD,
+      teamId: process.env.VITE_TEAM_ID,
+    },
+  },
+  rebuildConfig: {},
+  makers: [
+    // {
+    //   name: "@electron-forge/maker-squirrel",
+    //   config: {},
+    // },
+    {
+      name: "@electron-forge/maker-zip",
+      config: {
+        icon: "resources/icon",
+      },
+      platforms: ["darwin"],
+    },
+    // {
+    //   name: "@electron-forge/maker-deb",
+    //   config: {},
+    // },
+    // {
+    //   name: "@electron-forge/maker-rpm",
+    //   config: {},
+    // },
+  ],
+};
