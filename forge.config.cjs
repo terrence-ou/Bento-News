5efdff53b3025083963797d98168842c90079b36
terrence-ou <outerrencedl@gmail.com>
2024-12-04 10:25:09 -0500

    feat: DMG build updated

--- forge.config.cjs
+++ forge.config.cjs
@@ -17,24 +17,12 @@ module.exports = {
   },
   rebuildConfig: {},
   makers: [
-    // {
-    //   name: "@electron-forge/maker-squirrel",
-    //   config: {},
-    // },
     {
-      name: "@electron-forge/maker-zip",
+      name: "@electron-forge/maker-dmg",
       config: {
-        icon: "resources/icon",
+        name: "Bento News Installer",
+        icon: "resources/icon.icns",
       },
-      platforms: ["darwin"],
     },
-    // {
-    //   name: "@electron-forge/maker-deb",
-    //   config: {},
-    // },
-    // {
-    //   name: "@electron-forge/maker-rpm",
-    //   config: {},
-    // },
   ],
 };
